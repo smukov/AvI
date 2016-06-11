651d849300a911a38248644f6c27299097d73434
Miroslav Smukov <smukov@gmail.com>
2016-06-11 17:47:59 +0200

    added navigation to LoginPage

--- Ionic/app/pages/loginPage/loginPage.js
+++ Ionic/app/pages/loginPage/loginPage.js
@@ -1,16 +1,23 @@
 import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 import {Page1} from '../page1/page1';
 
 
 @Component({
-  templateUrl: 'build/pages/loginPage/loginPage.html',
+  templateUrl: 'build/pages/loginPage/loginPage.html'
 })
 export class LoginPage {
-  constructor() {
+  //this gets injected into constructor below, it's the order that matters
+  static get parameters() {
+    return [[NavController]];
+  }
+
+  constructor(nav, navParams) {
+    this.nav = nav;
     this.nextPage = Page1;
   }
 
   login(){
-
+    this.nav.setRoot(Page1);
   }
 }
