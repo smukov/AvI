61175a3d79e1ad0f9903f02bf581839e51546c72
Miroslav Smukov <smukov@gmail.com>
2016-06-18 13:53:08 +0200

    created profile header template, and added it to profile page

--- Android/app/src/main/java/com/thesis/smukov/anative/ProfileFragment.java
+++ Android/app/src/main/java/com/thesis/smukov/anative/ProfileFragment.java
@@ -6,18 +6,28 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
- * Created by smuko on 15-Jun-16.
+ * Created by smukov on 15-Jun-16.
  */
 public class ProfileFragment extends Fragment{
 
     View myView;
+    TextView profileName;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.profile_layout, container, false);
         return myView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        profileName = (TextView) view.findViewById(R.id.profile_name);
+        profileName.setText("Dr. Gregory House");
     }
 }
