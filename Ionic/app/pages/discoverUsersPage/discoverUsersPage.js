278ab2f962cfe33746053abcbb6e2026526ef2e7
Miroslav Smukov <smukov@gmail.com>
2016-07-18 23:45:08 +0200

    got the FABs above the slides

--- Ionic/app/pages/discoverUsersPage/discoverUsersPage.js
+++ Ionic/app/pages/discoverUsersPage/discoverUsersPage.js
@@ -1,11 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ProfileHeader} from '../components/profileHeader';
 import {ContactsService} from '../../services/contacts.service';
 
 
 @Component({
   templateUrl: 'build/pages/discoverUsersPage/discoverUsersPage.html',
-  directives: [ProfileHeader]
+  directives: [ProfileHeader],
+  queries: {
+    slider: new ViewChild('slider')
+  }
 })
 export class DiscoverUsersPage {
 
@@ -16,6 +19,24 @@ export class DiscoverUsersPage {
   constructor(contactsService) {
     this.contactsService = contactsService;
 
+    this.sliderOptions = {
+      loop: false
+    };
+
     this.users = this.contactsService.getNearbyUsers();
   }
+
+  onBtnDismissClicked(){
+    console.log('onBtnDismissClicked');
+  }
+
+  onBtnAcceptClicked(){
+    console.log('onBtnAcceptClicked');
+  }
+
+  onSlideChanged(){
+    let currentIndex = this.slider.getActiveIndex();
+    console.log(this.slider);
+    console.log("Current index is", currentIndex);
+ }
 }
