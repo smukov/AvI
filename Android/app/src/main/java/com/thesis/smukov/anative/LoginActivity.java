7109577c84cfa6cbf94032db8841459a3c2affa4
Miroslav Smukov <smukov@gmail.com>
2016-07-30 20:59:02 +0200

    added initial Auth0 code

--- Android/app/src/main/java/com/thesis/smukov/anative/LoginActivity.java
+++ Android/app/src/main/java/com/thesis/smukov/anative/LoginActivity.java
@@ -1,18 +1,40 @@
 package com.thesis.smukov.anative;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import com.auth0.core.UserProfile;
+import com.auth0.lock.Lock;
+import com.auth0.lock.LockActivity;
+import com.auth0.lock.LockContext;
 
 /**
  * A login screen that offers login via oAuth.
  */
 public class LoginActivity extends AppCompatActivity {
 
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            UserProfile userProfile = intent.getParcelableExtra(Lock.AUTHENTICATION_ACTION_PROFILE_PARAMETER);
+            String accessToken = intent.getParcelableExtra(Lock.AUTHENTICATION_ACTION_TOKEN_PARAMETER);
+            Log.i("smuk", "User " + userProfile.getName() + " logged in");
+
+            final Intent loggedInIntent =
+                    new Intent(getApplicationContext(), NavigationActivity.class);
+            startActivity(loggedInIntent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +65,15 @@ public class LoginActivity extends AppCompatActivity {
                 attemptLogin();
             }
         });
+
+        LockContext.configureLock(
+                new Lock.Builder()
+                        .loadFromApplication(getApplication())
+                        .closable(true));
+
+        final LocalBroadcastManager broadcastManager =
+                LocalBroadcastManager.getInstance(getApplicationContext());
+        broadcastManager.registerReceiver(receiver, new IntentFilter(Lock.AUTHENTICATION_ACTION));
     }
 
     /**
@@ -51,9 +82,12 @@ public class LoginActivity extends AppCompatActivity {
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        Intent intent = new Intent(this, NavigationActivity.class);
-        startActivity(intent);
-        finish();
+//        Intent intent = new Intent(this, NavigationActivity.class);
+//        startActivity(intent);
+//        finish();
+
+        Intent lockIntent = new Intent(this, LockActivity.class);
+        startActivity(lockIntent);
     }
 }
 
