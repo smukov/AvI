44083400b2aa603b26494cce2ec14dd51b849af2
Miroslav Smukov <smukov@gmail.com>
2016-09-05 20:45:36 +0200

    added auth0 logic for Token refresh

--- Ionic/app/app.js
+++ Ionic/app/app.js
@@ -64,6 +64,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      this.auth.startupTokenRefresh();
       this.preferencesService.initializePreferences();
     });
   }
--- Ionic/app/services/auth.service.js
+++ Ionic/app/services/auth.service.js
@@ -45,6 +45,8 @@ export class AuthService {
           return;
         }
 
+        console.log(profile);
+
         profile.user_metadata = profile.user_metadata || {};
         this.local.set('profile', JSON.stringify(profile));
         this.user = profile;
@@ -73,5 +75,80 @@ export class AuthService {
     this.local.remove('id_token');
     this.local.remove('refresh_token');
     this.zoneImpl.run(() => this.user = null);
+    // Unschedule the token refresh
+    this.unscheduleRefresh();
   }
+
+  scheduleRefresh() {
+  // If the user is authenticated, use the token stream
+  // provided by angular2-jwt and flatMap the token
+  let source = this.authHttp.tokenStream.flatMap(
+    token => {
+      // The delay to generate in this case is the difference
+      // between the expiry time and the issued at time
+      let jwtIat = this.jwtHelper.decodeToken(token).iat;
+      let jwtExp = this.jwtHelper.decodeToken(token).exp;
+      let iat = new Date(0);
+      let exp = new Date(0);
+
+      let delay = (exp.setUTCSeconds(jwtExp) - iat.setUTCSeconds(jwtIat));
+
+      return Observable.interval(delay);
+    });
+
+  this.refreshSubscription = source.subscribe(() => {
+    this.getNewJwt();
+  });
+}
+
+startupTokenRefresh() {
+  // If the user is authenticated, use the token stream
+  // provided by angular2-jwt and flatMap the token
+  if (this.authenticated()) {
+    let source = this.authHttp.tokenStream.flatMap(
+      token => {
+        // Get the expiry time to generate
+        // a delay in milliseconds
+        let now = new Date().valueOf();
+        let jwtExp = this.jwtHelper.decodeToken(token).exp;
+        let exp = new Date(0);
+        exp.setUTCSeconds(jwtExp);
+        let delay = exp.valueOf() - now;
+
+        // Use the delay in a timer to
+        // run the refresh at the proper time
+        return Observable.timer(delay);
+      });
+
+      // Once the delay time from above is
+      // reached, get a new JWT and schedule
+      // additional refreshes
+      source.subscribe(() => {
+        this.getNewJwt();
+        this.scheduleRefresh();
+      });
+  }
+}
+
+unscheduleRefresh() {
+  // Unsubscribe fromt the refresh
+  if (this.refreshSubscription) {
+    this.refreshSubscription.unsubscribe();
+  }
+}
+
+getNewJwt() {
+  // Get a new JWT from Auth0 using the refresh token saved
+  // in local storage
+  this.local.get('refresh_token').then(token => {
+    this.auth0.refreshToken(token, (err, delegationRequest) => {
+      if (err) {
+        alert(err);
+      }
+      this.local.set('id_token', delegationRequest.id_token);
+    });
+  }).catch(error => {
+    console.log(error);
+  });
+ }
 }
