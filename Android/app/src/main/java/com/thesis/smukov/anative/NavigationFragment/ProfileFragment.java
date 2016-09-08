95efad83ff012df5158e088d8f81c992167042d1
Miroslav Smukov <smukov@gmail.com>
2016-09-08 22:12:45 +0200

    loading and persisting user info data

--- Android/app/src/main/java/com/thesis/smukov/anative/NavigationFragment/ProfileFragment.java
+++ Android/app/src/main/java/com/thesis/smukov/anative/NavigationFragment/ProfileFragment.java
@@ -1,6 +1,8 @@
 package com.thesis.smukov.anative.NavigationFragment;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -11,7 +13,16 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.thesis.smukov.anative.Models.UserInfo;
 import com.thesis.smukov.anative.R;
+import com.thesis.smukov.anative.Store.UserInfoStore;
+import com.thesis.smukov.anative.Utils.DownloadImageTask;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by smukov on 15-Jun-16.
@@ -20,12 +31,15 @@ public class ProfileFragment extends BaseNavigationFragment {
 
     Boolean isEditable = false;
 
+    CircleImageView profileImage;
     TextView profileName;
     EditText txtEmployment;
     EditText txtEducation;
     EditText txtInterests;
     EditText txtKnowledgeable;
     EditText txtCurrentGoals;
+
+    UserInfo userInfo;
 
     @Nullable
     @Override
@@ -42,6 +56,8 @@ public class ProfileFragment extends BaseNavigationFragment {
         setTitle(getResources().getString(R.string.titleMyProfile));
         prepareFloatingActionButton();
 
+        profileImage = (CircleImageView) view.findViewById(R.id.profile_image);
+
         profileName = (TextView) view.findViewById(R.id.profile_name);
         profileName.setText("Dr. Gregory House");
 
@@ -51,6 +67,7 @@ public class ProfileFragment extends BaseNavigationFragment {
         txtKnowledgeable = (EditText) view.findViewById(R.id.txtKnowledgeable);
         txtCurrentGoals = (EditText) view.findViewById(R.id.txtCurrentGoals);
 
+        populateUserInfo();
         toggleEditMode(isEditable);
     }
 
@@ -67,6 +84,9 @@ public class ProfileFragment extends BaseNavigationFragment {
             public void onClick(View view) {
                 isEditable = !isEditable;
                 toggleEditMode(isEditable);
+                if(isEditable == false){
+                    storeUserInfo();
+                }
             }
         });
     }
@@ -83,5 +103,42 @@ public class ProfileFragment extends BaseNavigationFragment {
         }else{
             fab.setImageDrawable(ContextCompat.getDrawable(getActivity(), R.drawable.ic_create_black_24dp));
         }
+    }
+
+    private void populateUserInfo(){
+        userInfo = UserInfoStore.getUserInfo(getActivity());
+        if(!userInfo.getName().isEmpty()){
+            profileName.setText(userInfo.getName());
+        }
+        if(!userInfo.getPictureUrl().isEmpty()){
+            new DownloadImageTask(profileImage)
+                    .execute(userInfo.getPictureUrl());
+        }
+        if(!userInfo.getEmployment().isEmpty()){
+            txtEmployment.setText(userInfo.getEmployment());
+        }
+        if(!userInfo.getEducation().isEmpty()){
+            txtEducation.setText(userInfo.getEducation());
+        }
+        if(!userInfo.getInterests().isEmpty()){
+            txtInterests.setText(userInfo.getInterests());
+        }
+        if(!userInfo.getKnowledgeableIn().isEmpty()){
+            txtKnowledgeable.setText(userInfo.getKnowledgeableIn());
+        }
+        if(!userInfo.getCurrentGoals().isEmpty()){
+            txtCurrentGoals.setText(userInfo.getCurrentGoals());
+        }
+    }
+
+    private void storeUserInfo(){
+        userInfo.setName(profileName.getText().toString());
+        userInfo.setEmployment(txtEmployment.getText().toString());
+        userInfo.setEducation(txtEducation.getText().toString());
+        userInfo.setInterests(txtInterests.getText().toString());
+        userInfo.setKnowledgeableIn(txtKnowledgeable.getText().toString());
+        userInfo.setCurrentGoals(txtCurrentGoals.getText().toString());
+
+        UserInfoStore.storeUserInfo(getActivity(), userInfo);
     }
 }
---
+++ Android/app/src/main/java/com/thesis/smukov/anative/Utils/DownloadImageTask.java
@@ -0,0 +1,38 @@
+package com.thesis.smukov.anative.Utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+/**
+ * Created by Smukov on 08-Sep-16.
+ * from http://stackoverflow.com/questions/5776851/load-image-from-url
+ */
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
+    }
+}
